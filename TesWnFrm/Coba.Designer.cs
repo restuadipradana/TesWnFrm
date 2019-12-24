f8845325ffacbca218ccff4369c54796ca85eaa1
(SYF-109) Restu Adi Pradana <restu.adi@syf.ssbshoes.local>
2019-12-24 15:19:40 +0700

    Add project files.

---
+++ TesWnFrm/Coba.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace TesWnFrm
+{
+    partial class Coba
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHello = new System.Windows.Forms.TextBox();
+            this.btnHello = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtHello
+            // 
+            this.txtHello.Location = new System.Drawing.Point(72, 71);
+            this.txtHello.Name = "txtHello";
+            this.txtHello.Size = new System.Drawing.Size(100, 20);
+            this.txtHello.TabIndex = 0;
+            this.txtHello.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnHello
+            // 
+            this.btnHello.Location = new System.Drawing.Point(72, 115);
+            this.btnHello.Name = "btnHello";
+            this.btnHello.Size = new System.Drawing.Size(75, 23);
+            this.btnHello.TabIndex = 1;
+            this.btnHello.Text = "Hello";
+            this.btnHello.UseVisualStyleBackColor = true;
+            this.btnHello.Click += new System.EventHandler(this.btnHello_Click);
+            // 
+            // Coba
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 363);
+            this.Controls.Add(this.btnHello);
+            this.Controls.Add(this.txtHello);
+            this.Name = "Coba";
+            this.Text = "Baca Excel";
+            this.Load += new System.EventHandler(this.Coba_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHello;
+        private System.Windows.Forms.Button btnHello;
+    }
+}
+
---
+++ TesWnFrm/Coba.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TesWnFrm
+{
+    public partial class Coba : Form
+    {
+        public Coba()
+        {
+            InitializeComponent();
+        }
+
+        private void Coba_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHello_Click(object sender, EventArgs e)
+        {
+            txtHello.Text = "Hellow!";
+        }
+    }
+}
